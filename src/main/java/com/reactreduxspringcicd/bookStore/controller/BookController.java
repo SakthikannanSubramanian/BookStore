282b1fb7fea5b58eea1f111754a1cd5210f8e65f
Sakthikannan Subramanian <sakthikannan.subramanian@gmail.com>
2023-07-12 21:50:31 +0100

    Initial commit

---
+++ src/main/java/com/reactreduxspringcicd/bookStore/controller/BookController.java
@@ -0,0 +1,31 @@
+package com.reactreduxspringcicd.bookStore.controller;
+
+import com.reactreduxspringcicd.bookStore.dto.BookDto;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/books")
+public class BookController {
+
+    @GetMapping
+    public ResponseEntity<List<BookDto>> getBooks(){
+
+        BookDto book = BookDto.builder()
+                .title("My First book title")
+                .build();
+
+        List<BookDto> books = new ArrayList<>();
+        books.add(book);
+        return new ResponseEntity<>(books, HttpStatus.OK);
+
+    }
+
+}
---
+++ src/test/java/com/reactreduxspringcicd/bookStore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactreduxspringcicd.bookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
